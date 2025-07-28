cd42013c187f4cf71c53d3c8c75e905446028da2
David <david@10.0.2.15>
2025-07-28 13:57:18 +0200

    feat: Add exercise 2

--- src/ejerciciosBasicos/ConvertirEnmayusculas.java
+++ src/ejerciciosBasicos/ConvertirEnmayusculas.java
@@ -1,4 +1,21 @@
+/**
+ * Convertir strings a mayúsculas
+ *     Dada una lista de strings, convierte todos los elementos a mayúsculas usando map.
+ */
+
+
 package ejerciciosBasicos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConvertirEnmayusculas {
+     public static void main (String[] args){
+         List<String> palabras = new ArrayList<>(List.of("Maceta", "Persiana", "Carricoche", "Acuoso"));
+
+         palabras.stream().map(p -> p.toUpperCase()).forEach(System.out::println);
+     }
+
+
+
 }
